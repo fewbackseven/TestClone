ef6e9f412045e610c20a10aa7a47c1ecb1b69361
Vaishishtya_1 <Vaishishtya_1@DESKTOP-S4BTJVA>
2021-09-01 02:05:21 +0530

    First Page completed

---
+++ TestClone/TestClone/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestClone
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            //Image File_Image = new Image();
+            //File_Image.Source = ImageSource.FromFile("book1.png");
+            InitializeComponent();
+
+            
+
+        }
+
+        private void OnCreateBtnClicked(object sender,EventArgs e)
+        {
+            DisplayAlert("Do you like it?", "Lets continue", "cancel");
+        }
+        
+    }
+}
